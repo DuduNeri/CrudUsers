5d2f08b48886abca0fdf6c1052743c09c81041ec
DuduNeri <nerimartinseduardo@gmail.com>
2025-06-30 08:35:30 -0300

    Porta atualizada 8008

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use('/api', usersRoutes); 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8008;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
